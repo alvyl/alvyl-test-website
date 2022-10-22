1f04cf032d4cc53b8335d9ea24b394199b8720de
MUNDE SANJAY <38632534+im-crooked@users.noreply.github.com>
2022-10-23 01:50:43 +0530

    WIP-23-10-22

--- animate.js
+++ animate.js
@@ -17,3 +17,33 @@ function getPercentageScrolled(element) {
   );
   return Math.min(100, Math.max(0, percentageScrolled));
 }
+
+function myFunction() {
+  var element = document.getElementById("homepage");
+  console.log(element, "hello");
+  // element.classList.remove("sticky");
+}
+function isVisible(domElement) {
+  return new Promise((resolve) => {
+    const o = new IntersectionObserver(([entry]) => {
+      resolve(entry.intersectionRatio === 1);
+      o.disconnect();
+    });
+    o.observe(domElement);
+  });
+}
+
+myFunction();
+const asyncFunction = async () => {
+  const visible = await isVisible(document.querySelector("#testani"));
+  console.log(visible, "knkdsjncsdlncldsncdlsn");
+};
+
+$(window).scroll(function () {
+  if ($("#testani").visible()) {
+    console.log("visible");
+    $("homepage").removeClass("sticky");
+  } else {
+    $("#elementId").addClass("hp_buttons");
+  }
+});
